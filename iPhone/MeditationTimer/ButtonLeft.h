dcb30426aed64ea9cd61be20c5005a3f64e99f4d
Alyssa Reese <alyssa.reese@gmail.com>
2011-07-27 19:39:36 -0600

    Changes & fixes

---
+++ iPhone/MeditationTimer/ButtonLeft.h
@@ -0,0 +1,15 @@
+//
+//  ButtonLeft.h
+//
+//  Created by Alyssa Reese on 7/27/11.
+//  Copyright 2011 @aaReese. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <Foundation/Foundation.h>
+
+@interface ButtonLeft : NormalButton {
+
+}
+
+@end
---
+++ iPhone/MeditationTimer/ButtonLeft.m
@@ -0,0 +1,12 @@
+//
+//  ButtonLeft.m
+//
+//  Created by Alyssa Reese on 7/27/11.
+//  Copyright 2011 @aaReese. All rights reserved.
+//
+
+#import "ButtonLeft.h"
+
+@implementation ButtonLeft
+
+@end
